80083b12e3a10aa72c17c542d6b047666d222881
Alxsparker <alexandre.chalon@gmail.com>
2026-06-06 12:07:09 +0200

    Initial Commit

---
+++ VELVET SHOW/VELVET_SHOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  VELVET_SHOWApp.swift
+//  VELVET SHOW
+//
+//  Created by Alexandre CHALON on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct VELVET_SHOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
