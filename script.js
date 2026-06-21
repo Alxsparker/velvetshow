10dcaafc369695b9ace3cfb7c472f2a16918dc4d
Alxsparker <alexandre.chalon@gmail.com>
2026-06-21 23:12:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@
 // Keep the release asset name exactly: VELVET.SHOW.zip
 // ============================================================
 var VELVET_SHOW_DOWNLOAD_URL =
-var VELVET_SHOW_DOWNLOAD_URL =
   'https://github.com/Alxsparker/velvetshow/releases/download/v0.11-maestro-osc/VELVET.SHOW.zip';
 (function () {
   var downloadLinks = document.querySelectorAll('[data-download-link]');
